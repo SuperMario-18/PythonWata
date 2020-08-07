c055325f0d7ebdae45682a3a72cea857e77502aa
SuperMario-18 <68797979+SuperMario-18@users.noreply.github.com>
2020-08-07 15:30:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,48 @@ print(type(num))
 # <class 'float'> 
 print(3 % 2)
 # 1 is the remainder 
+
+num = 1 
+num *= 10
+print(num)
+# 10
+
+print(abs(-3))
+# 3
+
+print(round(3.77898))
+# 4
+
+print(round(3.75, 1))
+#Will round to the first decimal so 3.8 
+
+# Comparisons: 
+# Equal: 3 == 2
+# Not Equal: 3 != 2 
+# Greater Than: 3 > 2
+# Less Than: 3 < 2 
+# Greater or Equal: 3 >= 2
+# Less or Equal: 3 <= 2
+
+num_1 = 3
+num_2 = 2 
+
+print(num_1 == num_2)
+# False
+
+print(num_1 != num_2)
+# True 
+
+num_3 = '100'
+num_4 = '200' 
+
+print(num_3 + num_4)
+#100200
+
+num_3 = int(num_3)
+num_4 = int(num_4)
+
+print(num_3 + num_4)
+#300
+         
+ 
