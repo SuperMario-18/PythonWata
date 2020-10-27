d9a00966dacfdc399f6e882d5b1ceab61620e33a
SuperMario <manizaleno_18@hotmail.com>
2020-10-27 14:36:22 -0400

    10.27.20

--- main.py
+++ main.py
@@ -2,24 +2,32 @@
 
 
 
-hours_1 = int(input())*3600
-minute_1 = int(input())*60
-second_1 = int(input())
-hours_2 = int(input())*3600
-minute_2 = int(input())*60
-second_2 = int(input())
 
-scds_btw = abs((hours_1 + minute_1 + second_1) - (hours_2 + minute_2 + second_2))
-
-print(scds_btw)
-
-
-
-
-
-
-
-
-
+chicken = 23
+goat = 678
+pig = 1296
+cow = 3848
+sheep = 6769
+money = int(input())
+if money == 23:
+    print("1 Chicken")
+elif 23 < money < 678:
+    print(money//chicken, "Chickens")
+elif money == 678:
+    print("1 Goat")
+elif 678 < money < 1296:
+    print(money//goat, "Goats")
+elif money == 1296:
+    print("1 pig")
+elif 1296 < money < 3848:
+    print(money//pig, "Pigs")
+elif money == 3848:
+    print("1 Cow")
+elif 3848 < money < 6769:
+    print(money//cow, "Cows")
+elif money == 6769:
+    print("1 Sheep")
+else:
+    print(money//sheep, "Sheep")
 
 
